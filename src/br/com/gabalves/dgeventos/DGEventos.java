0651c5bf4a860c90a56b880188289f60b0a436e3
Natan <Natan@Natan-PC>
2017-02-28 18:57:13 -0300

    Updated DGEventos.java

--- src/br/com/gabalves/dgeventos/DGEventos.java
+++ src/br/com/gabalves/dgeventos/DGEventos.java
@@ -1,5 +1,23 @@
 package br.com.gabalves.dgeventos;
 
-public class DGEventos {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DGEventos extends JavaPlugin {
+	
+	public void onEnable() {
+		configure();
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	private void configure() {
+		if(this.getDataFolder().exists()) {
+			this.getDataFolder().mkdir();
+		}
+		
+		saveDefaultConfig();
+	}
 
 }
